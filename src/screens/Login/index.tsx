2a8ee1fc5a6de9755a2bb7b444a49fd008a85ae9
Jonatan Santos <jonatansilvasantos@outlook.com>
2025-07-30 10:51:35 -0300

    chore: instalado pacote para utilizar nativewind para estilização do projeto.

---
+++ src/screens/Login/index.tsx
@@ -0,0 +1,9 @@
+import { View, Text } from "react-native";
+
+export const Login = () => {
+  return (
+    <View className="flex-1 items-center justify-center bg-white">
+      <Text>Login Screen</Text>
+    </View>
+  );
+};
---
+++ tailwind.config.js
@@ -0,0 +1,24 @@
+const { colors } = require("./src/shared/colors");
+module.exports = {
+  content: ["./src/**/*.{ts,tsx}"],
+  presets: [require("nativewind/preset")],
+  theme: {
+    extend: {
+      fontFamily: {
+        roboto: ["Roboto", "sans-serif"],
+      },
+      fontSize: {
+        sm: "14px",
+        base: "16px",
+        lg: "18px",
+        xl: "20px",
+        "2xl": "24px",
+        "3xl": "30px",
+        "4xl": "36px",
+        "5xl": "48px",
+      },
+      colors,
+    },
+  },
+  plugins: [],
+};
