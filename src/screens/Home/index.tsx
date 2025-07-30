b8fd0d4348dc11cd04fd240251efcbdd8d9fe281
Jonatan Santos <jonatansilvasantos@outlook.com>
2025-07-30 13:51:45 -0300

    chore: Configurado as rotas públicas e privadas em arquivos diferentes. Criado screen de Home.

---
+++ src/screens/Home/index.tsx
@@ -0,0 +1,9 @@
+import { View, Text } from "react-native";
+
+export const Home = () => {
+  return (
+    <View className="flex-1 items-center justify-center bg-white">
+      <Text>Home Screen</Text>
+    </View>
+  );
+};
--- src/screens/Login/index.tsx
+++ src/screens/Login/index.tsx
@@ -1,10 +1,11 @@
-import { PublicStackParamList } from "@/routes";
+import { PublicStackParamsList } from "@/routes/PublicRoutes";
 import { useNavigation } from "@react-navigation/native";
 import { StackNavigationProp } from "@react-navigation/stack";
 import { View, Text, Button } from "react-native";
 
 export const Login = () => {
-  const navigation = useNavigation<StackNavigationProp<PublicStackParamList>>();
+  const navigation =
+    useNavigation<StackNavigationProp<PublicStackParamsList>>();
   return (
     <View className="flex-1 items-center justify-center bg-white">
       <Text>Login Screen</Text>
