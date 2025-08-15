264fcac99bf215caf39adc3503e5763d5dd0de2e
Jonatan Santos <jonatansilvasantos@outlook.com>
2025-08-15 15:05:31 -0300

    chore: criado AppError, para instância do error e tratamento. criado snackbar para visualização passando o contexto por toda aplicação. criado o hook para compartilhar a snackbar por toda aplicação. criado o interceptor na api para resposta do erro.

---
+++ src/shared/hooks/useErrorHandler.tsx
@@ -0,0 +1,22 @@
+import { useSnackbarContext } from "@/context/snackbar.context";
+import { AppError } from "../helpers/AppError";
+
+export const useErrorHandler = () => {
+  const { notify } = useSnackbarContext();
+
+  const handlerError = (error: unknown, defaultMessage?: string) => {
+    const isAppError = error instanceof AppError;
+
+    const message = isAppError
+      ? error.message
+      : defaultMessage ?? "Falha na requisição";
+
+    notify({
+      message,
+      messageType: "ERROR",
+    });
+  };
+  return {
+    handlerError,
+  };
+};
