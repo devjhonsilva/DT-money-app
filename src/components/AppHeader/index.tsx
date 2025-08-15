2b34997c839be6c641e0a882af557381e71432d2
Jonatan Santos <jonatansilvasantos@outlook.com>
2025-08-15 17:23:01 -0300

    chore: criado componente de bottom sheet como contexto para ser reaproveitado em outras telas.

--- src/components/AppHeader/index.tsx
+++ src/components/AppHeader/index.tsx
@@ -2,9 +2,11 @@ import { View, Text, Image, TouchableOpacity } from "react-native";
 import { MaterialIcons } from "@expo/vector-icons";
 import { colors } from "@/shared/colors";
 import { useAuthContext } from "@/context/auth.context";
+import { useBottomSheetContext } from "@/context/bottomsheet.context";
 
 export const AppHeader = () => {
   const { handleLogout } = useAuthContext();
+  const { openBottomSheet } = useBottomSheetContext();
   return (
     <View className="w-full flex-row p-8 justify-between items-center">
       <View>
@@ -20,7 +22,12 @@ export const AppHeader = () => {
           <Text className="text-gray-700 text-base">Sair da conta</Text>
         </TouchableOpacity>
       </View>
-      <TouchableOpacity className="bg-accent-brand w-[130px] h-[50px] items-center justify-center rounded-xl">
+      <TouchableOpacity
+        className="bg-accent-brand w-[130px] h-[50px] items-center justify-center rounded-xl"
+        onPress={() =>
+          openBottomSheet(<Text> formulário Nova Transação</Text>, 1)
+        }
+      >
         <Text className="text-white font-bold text-sm">Nova transação</Text>
       </TouchableOpacity>
     </View>
--- src/context/bottomsheet.context.tsx
+++ src/context/bottomsheet.context.tsx
@@ -3,8 +3,14 @@ import {
   FC,
   PropsWithChildren,
   useCallback,
+  useContext,
+  useRef,
   useState,
 } from "react";
+import BottomSheet, { BottomSheetScrollView } from "@gorhom/bottom-sheet";
+import { TouchableWithoutFeedback, View } from "react-native";
+import { set } from "react-hook-form";
+import { colors } from "@/shared/colors";
 
 interface BottomSheetContextType {
   openBottomSheet: (content: React.ReactNode, index: number) => void;
@@ -15,20 +21,65 @@ export const BottomSheetContext = createContext({} as BottomSheetContextType);
 
 export const BottomSheetProvider: FC<PropsWithChildren> = ({ children }) => {
   const [content, setContent] = useState<React.ReactNode | null>(null);
+  const bottomSheetRef = useRef<BottomSheet>(null);
+  const snapPoints = ["70%", "90%"];
+  const [index, setIndex] = useState(-1);
+  const [isOpen, setIsOpen] = useState(false);
 
   const openBottomSheet = useCallback(
     (NewContent: React.ReactNode, index: number) => {
+      setIndex(index);
+      setIsOpen(true);
       setContent(NewContent);
+      requestAnimationFrame(() => {
+        bottomSheetRef.current?.snapToIndex(index);
+      });
     },
     []
   );
 
   const closeBottomSheet = useCallback(() => {
+    setIndex(-1);
+    setIsOpen(false);
     setContent(null);
+    bottomSheetRef.current?.close();
   }, []);
+
+  const handleSheetChanges = useCallback((index: number) => {
+    if (index === -1) {
+      setIsOpen(false);
+    }
+  }, []);
+
   return (
     <BottomSheetContext.Provider value={{ openBottomSheet, closeBottomSheet }}>
       {children}
+
+      {isOpen && (
+        <TouchableWithoutFeedback onPress={closeBottomSheet}>
+          <View className="absolute inset-0 gb-black/70 z-1" />
+        </TouchableWithoutFeedback>
+      )}
+      <BottomSheet
+        ref={bottomSheetRef}
+        snapPoints={snapPoints}
+        style={{ zIndex: 2 }}
+        index={index}
+        enablePanDownToClose
+        onChange={handleSheetChanges}
+        backgroundStyle={{
+          backgroundColor: colors["background-secondary"],
+          borderTopLeftRadius: 32,
+          borderTopRightRadius: 32,
+          elevation: 9,
+        }}
+      >
+        <BottomSheetScrollView>{content}</BottomSheetScrollView>
+      </BottomSheet>
     </BottomSheetContext.Provider>
   );
+};
+
+export const useBottomSheetContext = () => {
+  return useContext(BottomSheetContext);
 };
